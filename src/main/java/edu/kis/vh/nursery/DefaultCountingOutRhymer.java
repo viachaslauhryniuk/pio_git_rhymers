42c3f6e125b80c60243f85c4a2b6d3716331a196
DanielKarolak <245838@edu.p.lodz.pl>
2024-05-15 19:20:16 +0200

    Poprawa konwencji nazewniczej pól klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,34 +1,28 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+public class DefaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    private int[] numbers = new int[12];
 
     public int total = -1;
-
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            numbers[++total] = in;
     }
-
     public boolean callCheck() {
         return total == -1;
     }
-
     public boolean isFull() {
         return total == 11;
     }
-
     protected int peekaboo() {
         if (callCheck())
             return -1;
-        return NUMBERS[total];
+        return numbers[total];
     }
-
     public int countOut() {
         if (callCheck())
             return -1;
-        return NUMBERS[total--];
+        return numbers[total--];
     }
-
 }
--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,19 +1,14 @@
 package edu.kis.vh.nursery;
+public class FIFORhymer extends DefaultCountingOutRhymer {
+public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
-public class FIFORhymer extends defaultCountingOutRhymer {
-
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-
-    @Override
-    public int countOut() {
+@Override
+public int countOut() {
         while (!callCheck())
-            temp.countIn(super.countOut());
-
+        temp.countIn(super.countOut());
         int ret = temp.countOut();
-
         while (!temp.callCheck())
-            countIn(temp.countOut());
-
+        countIn(temp.countOut());
         return ret;
-    }
+        }
 }
